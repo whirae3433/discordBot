7c964853dafd56c079c92db0975c7f313e38c68a
Whirae <whirae3433@gmail.com>
2025-07-24 13:48:58 +0900

    디코 채널 별로 분리

--- config/index.js
+++ config/index.js
@@ -1,16 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 
-const jsonDir = path.join(__dirname, 'json');
-const jsonFiles = fs.readdirSync(jsonDir).filter(file => file.endsWith('.json'));
-
+const baseDir = path.join(__dirname, '../discordChannel');
 const channelConfigMap = {};
 
-for (const file of jsonFiles) {
-  const config = require(path.join(jsonDir, file));
-  for (const [channelId, data] of Object.entries(config)) {
-    channelConfigMap[channelId] = data;
+// 폴더 내 json 파일 재귀 탐색
+function loadJsonFiles(dir) {
+  const entries = fs.readdirSync(dir, { withFileTypes: true });
+
+  for (const entry of entries) {
+    const fullPath = path.join(dir, entry.name);
+
+    if (entry.isDirectory()) {
+      // 폴더면 재귀탐색
+      loadJsonFiles(fullPath);
+    } else if (entry.isFile() && entry.name.endsWith('.json')) {
+      const config = require(fullPath);
+      for (const [channelId, data] of Object.entries(config)) {
+        channelConfigMap[channelId] = data;
+      }
+    }
   }
 }
+
+loadJsonFiles(baseDir);
 
 module.exports = channelConfigMap;
