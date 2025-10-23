17874a35f29ae897f11eac3c4f74d6b4568e53fa
Whirae <whirae3433@gmail.com>
2025-10-23 16:29:12 +0900

    구글토큰

--- utils/googleSheets.js
+++ utils/googleSheets.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
 const { google } = require('googleapis');
 
+function normalizeKey(key) {
+  if (!key) return undefined;
+  return key.includes('\\n')
+    ? key.replace(/\\n/g, '\n')
+    : key.replace(/\n/g, '\n');
+}
+
 const auth = new google.auth.GoogleAuth({
   credentials: {
     type: 'service_account',
     project_id: process.env.GOOGLE_PROJECT_ID,
     private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY
-      ? process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
-      : undefined,
+    private_key: normalizeKey(process.env.GOOGLE_PRIVATE_KEY),
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     client_id: process.env.GOOGLE_CLIENT_ID,
   },
